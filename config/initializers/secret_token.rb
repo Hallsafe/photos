179774de4cb6cd23e4599b51b9f3800a45fbd188
Joe Hall <Joseph.L.Hall@gmail.com>
2012-12-09 18:13:54 -0800

    Create a rails application and add collections.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photos::Application.config.secret_token = '0addc6fdc26f5cce1c08ebe24804cc391a97699f41e192833c2ff82eb92668edab210fd64a77b024658ba93fbb5fdcb4024dbd4b5335dea660818a12767e81df'
